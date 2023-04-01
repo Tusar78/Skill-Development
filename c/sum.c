c17d92221b02b9326cd627ac9635a365d3b1db5e
Tusar <tusarahmed141@gmail.com>
2023-04-01 15:07:08 +0600

    last update

--- c/sum.c
+++ c/sum.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    double a = 10.1, b = 2.147, sum;
+    int a, b, sum;
+
+    scanf("%d %d", &a, b);
 
     sum = a + b;
 
-    printf("Summation is = %lf\n", sum);
-    printf("Summation is = %0.2lf\n", sum);
-    printf("Summation is = %0.0lf\n", sum);
+    printf("Sum is: %d\n", sum);
+
     return 0;
 }
