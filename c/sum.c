21bdf69832e0bd08c59495e7f2c5bc9c447fdff3
Tusar <tusarahmed141@gmail.com>
2023-04-03 12:31:20 +0600

    Summation mul div sub using printf function

--- c/sum.c
+++ c/sum.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2;
+    double num1, num2, value;
+    char sign;
 
-    printf("Please Enter a Number: ");
-    scanf("%d", &num1);
-    printf("Please Enter another Number: ");
-    scanf("%d", &num2);
+    printf("Enter a number: ");
+    scanf("%lf", &num1);
+    printf("Enter another number: ");
+    scanf("%lf", &num2);
 
-    printf("%d + %d = %d\n", num1, num2, num1 + num2);
-    printf("%d - %d = %d\n", num1, num2, num1 - num2);
-    printf("%d * %d = %d\n", num1, num2, num1 * num2);
-    printf("%d / %d = %d\n", num1, num2, num1 / num2);
+    value = num1 + num2;
+    sign = '+';
+    printf("%lf %c %lf = %0.2lf\n", num1, sign, num2, value);
+
+    value = num1 - num2;
+    sign = '-';
+    printf("%lf %c %lf = %0.2lf\n", num1, sign, num2, value);
+
+    value = num1 * num2;
+    sign = '*';
+    printf("%lf %c %lf = %0.2lf\n", num1, sign, num2, value);
+
+    value = num1 / num2;
+    sign = '/';
+    printf("%lf %c %lf = %0.2lf\n", num1, sign, num2, value);
+
     return 0;
 }
