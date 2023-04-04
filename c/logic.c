51df349bb938efa8f05c88ee8fc8d69331ea59fb
Tusar <tusarahmed141@gmail.com>
2023-04-04 12:08:11 +0600

    conditio

---
+++ c/logic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+ {
+     int n;
+     n = -10;
+     if(n<0) {
+        printf("This number is Negative\n");
+     }
+     if (n > 0) {
+       printf("This number is Positive\n");
+     }
+     if (n == 0) {
+       printf("This number is Zero\n");
+     }
+     return 0;
+ }
