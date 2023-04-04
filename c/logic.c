a025915529e757142c8489040eacff314460a1f1
Tusar <tusarahmed141@gmail.com>
2023-04-04 12:47:15 +0600

    Last update

--- c/logic.c
+++ c/logic.c
@@ -2,13 +2,12 @@
 
 int main()
  {
-     int n;
-     n = 10;
-     if(n < 30) {
-        printf("This number is less then 30\n");
-     }
-     else if (n < 50) {
-       printf("This number is less then 50\n");
+     int number, reminder;
+     printf("Enter your number: ");
+     scanf("%d", &number);
+     reminder = number % 2;
+     if(reminder == 0) {
+        printf("This is even Number.\n");
      }
      return 0;
  }
