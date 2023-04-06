3bf21ee0ae75f3caa4312cd59b85b24852a4d774
Tusar <tusarahmed141@gmail.com>
2023-04-06 16:25:13 +0600

    Last update

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,3 +1,11 @@
-const myObj = {
-  name: "Tusar",
-};
+// "use strict"
+
+const outer = 'Out';
+
+function myFunc() {
+  const inner = 'Inner';
+  console.log(outer, inner);
+}
+
+myFunc();
+console.log(outer, inner);
