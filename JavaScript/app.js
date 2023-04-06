d41540c7a95b9161edf93cea4472cf5c5ab55f36
Tusar <tusarahmed141@gmail.com>
2023-04-07 00:07:50 +0600

    Last update

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,11 +1,27 @@
 // "use strict"
 
-const outer = 'Out';
+// const outer = 'Out';
 
-function myFunc() {
-  const inner = 'Inner';
-  console.log(outer, inner);
-}
+// function myFunc() {
+//   const inner = 'Inner';
+//   console.log(outer, inner);
+// }
 
-myFunc();
-console.log(outer, inner);
+// myFunc();
+// console.log(outer, inner);
+
+// if (true) {
+//   var varVariable = "This is var";
+// }
+
+// console.log(varVariable);
+
+// if (true) {
+//   let letVariable = "This is let";
+//   console.log(letVariable);
+//   letVariable = "";
+//   console.log(letVariable);
+//   console.log(varVariable);
+// }
+
+// console.log(letVariable);
--- c/logic.c
+++ c/logic.c
@@ -2,12 +2,34 @@
 
 int main()
  {
-     int number, reminder;
-     printf("Enter your number: ");
-     scanf("%d", &number);
-     reminder = number % 2;
-     if(reminder == 0) {
-        printf("This is even Number.\n");
-     }
-     return 0;
+    int num1, num2, sign, dividend, divisor, count, result;
+
+    printf("Enter dividend Number: ");
+    scanf("%d", &num1);
+    printf("Enter divisor Number: ");
+    scanf("%d", &num2);
+
+    sign = num1 < 0 ? -1 : 1;
+    if(num1 < 0) {
+        num1 = -num1;
+    }
+    if(num2 < 0) {
+        num2 = -num2;
+    }
+    dividend = num1;
+    divisor = num2;
+
+    count = dividend;
+    while(count >= divisor) {
+        count = count - divisor;
+    }
+
+    result = sign * count;
+
+    if(result == 1) {
+        printf("The number is Odd");
+    } else {
+        printf("The number is Even");
+    }
+
  }
