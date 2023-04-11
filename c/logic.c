80a977a1c090533ea303a4ac2aef0a0cc7bd00be
Tusar <tusarahmed141@gmail.com>
2023-04-12 01:41:50 +0600

    Again coded custom remainder

--- c/logic.c
+++ c/logic.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 
 int main()
@@ -32,3 +33,43 @@ int main()
         printf("The number is Even");
     }
  }
+*/
+
+#include<stdio.h>
+
+int main() {
+    int num1, num2, dividend, divisor, count, sign, result;
+
+    printf("Enter Dividend Number: ");
+    scanf("%d", &num1);
+    printf("Enter Divisor Number: ");
+    scanf("%d", &num2);
+
+    if(num1 < 0) {
+        num1 = - num1;
+    }
+
+    if(num2 < 0) {
+        num2 = -num2;
+    }
+
+    sign = sign < 0 ? -1 : 1;
+
+    dividend = num1;
+    divisor = num2;
+
+    count = dividend;
+
+    while(count >= divisor) {
+        count = count - divisor;
+    }
+
+    result = count * sign;
+
+    if(result == 1) {
+        printf("This is Odd Number!");
+    } else {
+        printf("This is Even Number!");
+    }
+
+}
