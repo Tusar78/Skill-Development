aaed1a3e57201d416797d5bc13df025a148cd388
Tusar <tusarahmed141@gmail.com>
2023-06-17 18:56:07 +0600

    Practicing comment and also escape character

---
+++ c/4 First C Program/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    // Print my name and also i'm using escape character \n
+    printf("\aTusar\n");
+    printf("\rBangladesh University of Business Technology");
+    getch();
+}
