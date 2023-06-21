a4a61d85122bcc3ee9e549a8169411c65e86eda5
Tusar <tusarahmed141@gmail.com>
2023-06-21 17:57:15 +0600

    Convert uppercase to lowercase and also vise varsa

--- c/07 LC to UC and UC to LC with or without method/main.c
+++ c/07 LC to UC and UC to LC with or without method/main.c
@@ -2,6 +2,20 @@
 
 int main() {
     char name;
+    int toLowerCase;
+
+    printf("Enter your UPPERCASE letter: ");
+    scanf("%c", &name);
+
+    toLowerCase = name + 32;
+
+    printf("Your lowercase letter is: %c", toLowerCase);
+    getch();
+}
+
+/*
+int main() {
+    char name;
     int toUpperCase;
 
     printf("Enter your lowercase letter: ");
@@ -12,3 +26,4 @@ int main() {
     printf("Converted lowercase %c to uppercase %c", name, toUpperCase);
     //printf("Character %c to ASCII %c", name, toUpperCase);
 }
+*/
