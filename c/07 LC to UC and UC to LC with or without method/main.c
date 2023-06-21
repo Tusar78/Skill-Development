3c31529ba9d8df56fafb5c1549805a7756534ab8
Tusar <tusarahmed141@gmail.com>
2023-06-21 19:51:30 +0600

    Added addition converting program

--- c/07 LC to UC and UC to LC with or without method/main.c
+++ c/07 LC to UC and UC to LC with or without method/main.c
@@ -1,5 +1,33 @@
 #include<stdio.h>
 
+/*
+    1. Write a program to convert uppercase to lower and lowercase to uppercae
+*/
+
+int main() {
+    char c, toLower, toUpper;
+
+    printf("Type any character: ");
+    scanf("%c", &c);
+
+    if(c >= 65 && c <= 96) {
+        toLower = c + 32;
+        printf("Lowercase Letter is: %c", toLower);
+    } else if (c >= 97 && c <= 122) {
+        toUpper = c - 32;
+        printf("Uppercase Letter is: %c", toUpper);
+    } else {
+        printf("\aWrong Character!");
+    }
+
+    getch();
+
+    return 0;
+}
+
+
+/*
+
 int main() {
 	char upper, lower;
 
@@ -10,8 +38,6 @@ int main() {
 
 	printf("Your lowercase Letter is: %c", lower);
 }
-
-/*
 
 int main() {
     char lower, upper;
