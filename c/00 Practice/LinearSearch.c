14c7e3cda591521f147e58e6e4073ed395be1f0e
Tusar <tusarahmed141@gmail.com>
2023-08-28 13:58:37 +0600

    Implement linear search algorithm

--- c/00 Practice/LinearSearch.c
+++ c/00 Practice/LinearSearch.c
@@ -2,10 +2,40 @@
 
 int main() {
     int x[] = {5, 7, 9, 2, 3};
+    int n = sizeof(x) / sizeof(x[0]);
     int k;
 
     printf("Enter your desire number: ");
     scanf("%d", &k);
 
+    printf("\n");
 
+    int found = 0;
+
+    /*
+    for(int i = 0; i < n; i++) {
+        if(k == x[i]) {
+            printf("Your Search Number Position is: %d", i);
+
+            found++;
+            break;
+        }
+    }*/
+
+    int i = 0;
+    while(i < n) {
+
+        if(k == x[i]) {
+            printf("Your Search Number Position is: %d", i);
+
+            found++;
+            break;
+        }
+        i++;
+    }
+
+    if(found == 0) {
+        printf("Your Search Number is Not Found");
+    }
+    getch();
 }
