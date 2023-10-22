6c5432a23db9413a26d9a6fde0e09479d094a7a0
Tusar <tusarahmed141@gmail.com>
2023-10-22 14:46:11 +0600

    Practice variable

--- JavaScript/Beginners_JS/assets/js/variable.js
+++ JavaScript/Beginners_JS/assets/js/variable.js
@@ -8,3 +8,15 @@ console.log(message);
 let user = 'John'
   ,  age = 25
   ,  message2 = 'Hi There!';
+
+
+let admin;
+let name;
+
+name = 'john';
+admin =  name;
+alert(admin);
+
+const OUR_PLANET = 'Earth';
+let ourPlanetName = 'Earth';
+let currentUserName = 'Anonymous';
