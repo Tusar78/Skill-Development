7284584414236bf776d7de61bf4f8e457fb34c79
Tusar <tusarahmed141@gmail.com>
2023-10-22 14:52:39 +0600

    Age calculate is ready

--- JavaScript/Beginners_JS/assets/js/variable.js
+++ JavaScript/Beginners_JS/assets/js/variable.js
@@ -1,22 +1,29 @@
 let message;
 
-message = 'Hello!';
+message = "Hello!";
 console.log(message);
 
 // Multiple variable in a line.
 
-let user = 'John'
-  ,  age = 25
-  ,  message2 = 'Hi There!';
-
+let user = "John",
+  age = 25,
+  message2 = "Hi There!";
 
 let admin;
 let name;
 
-name = 'john';
-admin =  name;
-alert(admin);
+name = "john";
+admin = name;
+// alert(admin);
 
-const OUR_PLANET = 'Earth';
-let ourPlanetName = 'Earth';
-let currentUserName = 'Anonymous';
+const OUR_PLANET = "Earth";
+let ourPlanetName = "Earth";
+let currentUserName = "Anonymous";
+
+function ageCalculate(birthDate) {
+  let date = new Date();  
+  return date.getFullYear() - birthDate;
+}
+
+const myAge = ageCalculate(1999);
+console.log(myAge);
