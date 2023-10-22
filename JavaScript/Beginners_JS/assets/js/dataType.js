61a1ed43fb5826179b898f34d59e91e2d113fd7d
Tusar <tusarahmed141@gmail.com>
2023-10-22 18:04:49 +0600

    Try to practice type convertion

--- JavaScript/Beginners_JS/assets/js/dataType.js
+++ JavaScript/Beginners_JS/assets/js/dataType.js
@@ -1,2 +1,38 @@
-const exception = Math.pow(NaN, 0);
-console.log(exception);
+// const exception = Math.pow(NaN, 0);
+// console.log(exception);
+
+// const message = `Two Plus Three is equal to ${2+3}`;
+// console.log(message);
+
+// let isGreater = 5 <= 5;
+// console.log(isGreater);
+
+// const name1 = undefined;
+// let name2;
+// var name3;
+
+// console.log(name1, name2, name3);
+// console.log(typeof 10n);
+
+// console.log(typeof Symbol('id'));
+
+// console.log(typeof null);
+
+// console.log(typeof alert)
+
+// let message = prompt("Enter your message");
+// console.log(message);
+// console.log(typeof message);
+
+// let isStudent = confirm("Are your Student?");
+
+// if (isStudent) {
+//   console.log('Go to the school');
+// } else {
+//   console.log('State at home!');
+// }
+
+// Type conversion
+let value = true;
+value = String(value)
+console.log(typeof value);
