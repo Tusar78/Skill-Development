aca86472db0ebfe381b1c1941320a0ff61c364f8
Tusar <tusarahmed141@gmail.com>
2023-10-24 00:28:23 +0600

    PRactice opreator

---
+++ JavaScript/Beginners_JS/assets/js/operators.js
@@ -0,0 +1,37 @@
+// let x = 5;
+// x = -x;
+// console.log(x);
+
+// console.log(45%30);
+
+
+// let a = 2;
+// let b = 3;
+// console.log(8 ** (1/3));
+
+// let a = true;
+// console.log(+a);
+
+// let b = '554';
+// console.log(+b);
+
+
+// let banana = '2';
+// let oranges = '3';
+
+// console.log(+banana + +oranges);
+
+// let a = 1;
+// let b = 2;
+
+// let c = 3 - (a = b + 1);
+// console.log(c);
+
+// let a, b, c;
+// a = b = c = 2+4;
+
+// console.log(a, b, c);
+
+// let counter = 1;
+// let result = ++counter;
+// console.log(result);
