44d9a875db6b27c82f9af09c0a9785a6d3a33fbc
Tusar <tusarahmed141@gmail.com>
2023-10-24 11:41:09 +0600

    Learn a gratly about comparison operator

---
+++ JavaScript/Beginners_JS/assets/js/comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1); //true
+// console.log(2 == 1); //false
+// console.log(2 != 1); //true
+
+let result = 54 != 54;
+// console.log(result);
+
+// console.log('z' > 'a');
+
+let a = 0;
+console.log( Boolean(a) ); // false
+
+let b = "0";
+console.log( Boolean(b) ); // true
+
+console.log(a == b); // true!
+
+console.log(undefined == undefined);
