0caebb4cbda45f8b8dca1ade5daa6815cbfd85ee
Tusar <tusarahmed141@gmail.com>
2023-10-24 21:49:16 +0600

    Minimum and maximum number is done

--- JavaScript/Problem/assets/js/large-small-number.js
+++ JavaScript/Problem/assets/js/large-small-number.js
@@ -1,10 +1,12 @@
 // Write a program to find max and min number
 
-let numbers = [1,2,3];
-// for (let i = 0; i < 3; i++) {
-//   let num = +prompt(`Enter the number ${i+1}`);
-//   numbers.push(num);
-// }
+let numbers = [];
+for (let i = 0; i < 3; i++) {
+  let num = +prompt(`Enter the number ${i+1}`);
+  numbers.push(num);
+}
+
+console.log(numbers);
 
 let minMax = prompt("Write min or max: ");
 minMax = minMax.toLowerCase();
@@ -17,13 +19,36 @@ function isMin([first, second, third]) {
     min = second;
   } else if (third < first && third < second) {
     min = third;
+  } else {
+    console.log('Equal');
   }
+
+  return min;
+}
+
+function isMax([first, second, third]) {
+  let max;
+  if (first > second && first > third) {
+    max = first;
+  } else if (second > first && second > third) {
+    max = second;
+  } else if (third > first && third > second) {
+    max = third;
+  } else {
+    console.log('Equal');
+  }
+
+  return max;
 }
 
 function minOrMax(numbers, minMax) {
   let result;
   if (minMax == 'min') {
-    result = isMin(numbers)
+    result = isMin(numbers);
+  } else if (minMax == 'max') {
+    result = isMax(numbers);
+  } else {
+    alert('Invalid attribute!');
   }
 
   return result;
