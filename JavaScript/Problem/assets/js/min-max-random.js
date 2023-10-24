036030ec808b35a98dda030d1127332f77c4f801
Tusar <tusarahmed141@gmail.com>
2023-10-24 19:42:11 +0600

    Solve a simple problem called min to max random number

--- JavaScript/Problem/assets/js/min-max-random.js
+++ JavaScript/Problem/assets/js/min-max-random.js
@@ -1,2 +1,21 @@
 // Random Number Generator between Minimum to Maximum
 
+// Get Number from user
+let min = prompt("Enter Minimum Number: ");
+let max = prompt("Enter Maximum Number: ");
+
+const randomInteger = (min, max) => {
+  min = Number.parseFloat(min);
+  max = Number.parseFloat(max);
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+if (min == null || max == null) {
+  alert("Please enter the number");
+} else if (isNaN(min) || isNaN(max)) {
+  alert("Please enter valid number");
+} else {
+  let result = randomInteger(min, max);
+  console.log(result);
+}
