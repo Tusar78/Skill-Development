ff511820b93a004beeaab287ddd2a3d9274cba94
Tusar <tusarahmed141@gmail.com>
2023-10-25 12:26:19 +0600

    Try to practice switch

---
+++ JavaScript/Beginners_JS/assets/js/switch.js
@@ -0,0 +1,16 @@
+let x = 1.125;
+
+switch (x) {
+  default:
+    console.log("Mile nai!");
+    break;
+  case 1.125:
+    console.log("This is Good");
+    break;
+  case 1.125:
+    console.log("This is Good");
+    break;
+  case 1.25:
+    console.log("This is very awesome");
+    break;
+}
