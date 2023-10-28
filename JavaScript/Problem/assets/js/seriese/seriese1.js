266d00ceb9d94efb336b8632a7c4043a8bd81aa5
Tusar <tusarahmed141@gmail.com>
2023-10-28 21:54:19 +0600

    Seriese one is done

---
+++ JavaScript/Problem/assets/js/seriese/seriese1.js
@@ -0,0 +1,20 @@
+//Series  
+// 1 + 2 + 3 + 4 + .... + n;
+
+let num = +prompt("Enter any number: ");
+
+const getSeries = num => {
+  let sum = 0;
+  let series = '';
+
+  for (let i = 1; i <= num; i++) {
+      sum += i;
+      series += i + ' + ';
+  }
+
+  return {sum, series};
+}
+
+const {sum, series} = getSeries(num);
+console.log(series.slice(0, -2));
+console.log(sum);
