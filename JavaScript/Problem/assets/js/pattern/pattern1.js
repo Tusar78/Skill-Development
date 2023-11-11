bd3add3136199f22cc54c7fe787a9dc18b04cfe4
Tusar <tusarahmed141@gmail.com>
2023-11-11 20:40:53 +0600

    Pattern one is done

---
+++ JavaScript/Problem/assets/js/pattern/pattern1.js
@@ -0,0 +1,23 @@
+// Pattern 1
+/**
+ 1
+ 1 2
+ 1 2 3
+ */
+
+ let n = +prompt("How Many Row you need?");
+
+ const patternOne = n => {
+  let pattern = '';
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      pattern += j + ' ';
+    }
+    pattern += '\n';
+  }
+
+  return {pattern}
+ }
+
+ const {pattern} = patternOne(n);
+console.log(pattern);
