e1bc7ab895f7636eb634e3d25fb92997f3667327
Tusar <tusarahmed141@gmail.com>
2023-11-13 14:05:04 +0600

    Another type of pattern done

--- JavaScript/Problem/assets/js/pattern/pattern5.js
+++ JavaScript/Problem/assets/js/pattern/pattern5.js
@@ -6,6 +6,32 @@ A B C
 A B C D
  */
 
+// let n = +prompt("How many row you want to show?");
+
+// const patternFive = n => {
+//   let pattern = '';
+
+//   for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= i ; j++) {
+//       pattern += String.fromCharCode(j+64) + ' ';
+//     }    
+//     pattern += "\n";
+//   }
+
+//   return {pattern};  
+// }
+
+// const {pattern} = patternFive(n);
+// console.log(pattern);
+
+// Pattern 5.1
+/**
+A
+B B
+C C C
+D D D D
+ */
+
 let n = +prompt("How many row you want to show?");
 
 const patternFive = n => {
