4e3abe855bb6d23c04521abde2c3e8672087b745
Tusar <tusarahmed141@gmail.com>
2023-11-14 14:48:45 +0600

    Box pattern is done

---
+++ JavaScript/Problem/assets/js/pattern/pattern8.js
@@ -0,0 +1,32 @@
+// Pattern Type
+/*
+x x x x x
+x       x
+x       x
+x       x
+x x x x x
+
+*/
+
+let rows = +prompt(
+  "Enter How Many Rows You Want to see, 'Press between 1 to 9'"
+);
+
+const patternOne = (n) => {
+  let pattern = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (i == 1 || i == n || j == 1 || j == n) {
+        pattern += "x" + " ";
+      } else {
+        pattern += "  ";
+      }
+    }
+    pattern += "\n";
+  }
+
+  return { pattern };
+};
+
+let {pattern} = patternOne(rows);
+console.log(pattern);
