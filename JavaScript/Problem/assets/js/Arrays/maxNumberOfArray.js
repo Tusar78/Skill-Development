69db0fd4bbce2d74997070c22ec0aac9435ead4a
Tusar <tusarahmed141@gmail.com>
2023-11-15 14:49:14 +0600

    Sum of array is done

--- JavaScript/Problem/assets/js/Arrays/maxNumberOfArray.js
+++ JavaScript/Problem/assets/js/Arrays/maxNumberOfArray.js
@@ -1,17 +1,36 @@
 // Write a program to find max number in an array
 
+// const arr = [4, 7, 8, 10, 7, 5, 97, 14, 8];
+
+// const maxNumber = arr => {
+//   let max = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (max < arr[i]) {
+//       max = arr[i];
+//     }
+//   }
+
+//   return {max}
+// }
+
+// let {maxVal} = maxNumber(arr);
+// console.log(maxVal);
+
+// Write a program to Sorting ascending order in an array
+
 const arr = [4, 7, 8, 10, 7, 5, 97, 14, 8];
 
 const maxNumber = arr => {
-  let max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (max < arr[i]) {
-      max = arr[i];
+  let sort = [];
+  let small = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (small < arr[i]) {
+      sort.push(arr[i])
     }
   }
 
-  return {max}
+  return {sort}
 }
 
-let {maxVal} = maxNumber(arr);
-console.log(maxVal);
+let {sort} = maxNumber(arr);
+console.log(sort);
