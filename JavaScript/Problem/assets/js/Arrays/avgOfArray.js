719a5d1ec0e181ffcef50521e4724f1e8f8900e0
Tusar <tusarahmed141@gmail.com>
2023-11-15 17:49:54 +0600

    Averge of all array elements

---
+++ JavaScript/Problem/assets/js/Arrays/avgOfArray.js
@@ -0,0 +1,21 @@
+// Write a program to sum of array elements
+
+const arr = [4, 7, 8, 1, 47, 57, 41, 11];
+
+const sumOfArray = (arr) => {
+  let sum = 0;
+  let display = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (i != arr.length-1) {
+      display += arr[i] + " + ";
+    } else {
+      display += arr[i];
+    }
+    sum += arr[i];
+  }
+
+  return { sum, display };
+};
+
+const { sum, display } = sumOfArray(arr);
+console.log(`${display} = ${sum}. avg : (${sum} / 2) = ${sum / 2}`);
