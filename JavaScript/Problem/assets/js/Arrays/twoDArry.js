3ea8c4160ab8a6911042df0e64c94200c72c2b01
Tusar <tusarahmed141@gmail.com>
2023-11-16 03:49:34 +0600

    Print 2d array

---
+++ JavaScript/Problem/assets/js/Arrays/twoDArry.js
@@ -0,0 +1,7 @@
+let twoDArray = [[10, 20, 30],[40, 45, 55]];
+
+for (let i = 0; i <twoDArray.length; i++) {
+  for (let j = 0; j < twoDArray[i].length; j++) {
+    console.log(twoDArray[i][j]);
+  }  
+}
