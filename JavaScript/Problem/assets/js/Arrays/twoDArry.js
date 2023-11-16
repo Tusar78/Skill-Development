b071541367ef0119b66fb86173b9490278907953
Tusar <tusarahmed141@gmail.com>
2023-11-16 21:20:46 +0600

    So far last update

--- JavaScript/Problem/assets/js/Arrays/twoDArry.js
+++ JavaScript/Problem/assets/js/Arrays/twoDArry.js
@@ -1,8 +1,19 @@
-let twoDArray = [[10, 20, 30],[40, 45, 55]];
+// let twoDArray = [[10, 20, 30],[40, 45, 55]];
 
-for (let i = 0; i <twoDArray.length; i++) {
-  for (let j = 0; j < twoDArray[i].length; j++) {
-    console.log(twoDArray[i][j]);
+// for (let i = 0; i <twoDArray.length; i++) {
+//   for (let j = 0; j < twoDArray[i].length; j++) {
+//     console.log(twoDArray[i][j]);
+//   }  
+// }
+
+
+// Insert 2D Array
+let simpleMatrix = [[], [], []];
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    let n = +prompt('Enter random number');
+    simpleMatrix[i][j] = n;
   }  
 }
 
+console.log(simpleMatrix);
