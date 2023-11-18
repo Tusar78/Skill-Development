f6e0aed939e5c80967a69dfbce7dcb12a5bd9677
Tusar <tusarahmed141@gmail.com>
2023-11-18 23:34:57 +0600

    Done by another problem called array to string

---
+++ JavaScript/Problem/assets/js/w3c/Array/Problem5.js
@@ -0,0 +1,21 @@
+// 5. Write a simple JavaScript program to join all elements of the following array into a string.
+
+// Way 01 = Loop
+const colors = ["Red", "Green", "White", "Black"];
+
+const arrayToString = (arr, s) => {
+  let color = '';
+  let arrLength = arr.length-1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arrLength > i) {
+      color += arr[i] + `${s}`; 
+    } else {
+      color += arr[i]; 
+    }
+  }
+
+  return {color}
+}
+
+const {color} = arrayToString(colors, '+');
+console.log(color);
