7f886b4301b8b081d9aff9e465bdb61e72792f75
Tusar <tusarahmed141@gmail.com>
2023-11-18 23:09:20 +0600

    Problem four is solved

--- JavaScript/Problem/assets/js/w3c/Array/Problem4.js
+++ JavaScript/Problem/assets/js/w3c/Array/Problem4.js
@@ -3,14 +3,32 @@
 // Way 01 => Loop
 const myArray = [4, 7, 8, 9, 87];
 
+// let getSubArray = (arr, n = 1) => {
+//   let newArray = [];
+//   let initial = arr.length - n;
+
+//   if (arr.length != 0 && n <= arr.length) {
+//     for (let i = initial; i < arr.length; i++) {
+//       newArray[i-initial] = arr[i];
+//     }
+//   }
+
+//   if (newArray.length == 1) {
+//     return newArray[0];
+//   } else {
+//     return newArray;
+//   }
+// };
+
+// const result = getSubArray(myArray, 4);
+// console.log(result);
+
+// Way 02 => Slice
 let getSubArray = (arr, n = 1) => {
   let newArray = [];
-  let initial = arr.length - n;
 
   if (arr.length != 0 && n <= arr.length) {
-    for (let i = initial; i < arr.length; i++) {
-      newArray[i-initial] = arr[i];
-    }
+    newArray = arr.slice(-n)
   }
 
   if (newArray.length == 1) {
@@ -20,5 +38,5 @@ let getSubArray = (arr, n = 1) => {
   }
 };
 
-const result = getSubArray(myArray, 4);
+const result = getSubArray(myArray, 3);
 console.log(result);
