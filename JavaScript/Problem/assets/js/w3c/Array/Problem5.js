4974b65577371a4547621d89a5def071e630236a
Tusar <tusarahmed141@gmail.com>
2023-11-19 00:23:13 +0600

    Problem five is solve

--- JavaScript/Problem/assets/js/w3c/Array/Problem5.js
+++ JavaScript/Problem/assets/js/w3c/Array/Problem5.js
@@ -3,16 +3,27 @@
 // Way 01 = Loop
 const colors = ["Red", "Green", "White", "Black"];
 
+// const arrayToString = (arr, s) => {
+//   let color = '';
+//   let arrLength = arr.length-1;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arrLength > i) {
+//       color += arr[i] + `${s}`; 
+//     } else {
+//       color += arr[i]; 
+//     }
+//   }
+
+//   return {color}
+// }
+
+// const {color} = arrayToString(colors, '+');
+// console.log(color);
+
+// Way 01 = join
 const arrayToString = (arr, s) => {
-  let color = '';
-  let arrLength = arr.length-1;
-  for (let i = 0; i < arr.length; i++) {
-    if (arrLength > i) {
-      color += arr[i] + `${s}`; 
-    } else {
-      color += arr[i]; 
-    }
-  }
+  let color;
+  color = arr.join(s);
 
   return {color}
 }
