b0423303d18e012e837c4264d43bd60e7c27fb7d
Tusar <tusarahmed141@gmail.com>
2023-11-22 01:07:59 +0600

    Raw JavaScript Inheritance

--- JavaScript/Think-In-JS/Prototype/class.js
+++ JavaScript/Think-In-JS/Prototype/class.js
@@ -11,3 +11,31 @@
 
 // const f = function Person() {};
 // console.dir(f);
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+function Player(name, age, type, country) {
+  Person.call(this);
+  this.name = name;
+  this.age = age;
+  this.type = type;
+  this.country = country;
+}
+
+Person.prototype = {
+  eat() {
+    console.log(`${name} can eat.`);
+  },
+  sleep() {
+    console.log(`${name} can sleep`);
+  }
+}
+
+Player.prototype = Object.create(Person.prototype);
+Player.prototype.constructor = Player
+
+const Sakib = new Player('Sakib', 37, 'All rounder', 'BD');
+console.log(Sakib);
