398d3655e6a9dc9dae7b4afb09108827da65a660
Tusar <tusarahmed141@gmail.com>
2023-11-22 00:23:37 +0600

    I going to exploring inheritance

---
+++ JavaScript/Think-In-JS/Prototype/class.js
@@ -0,0 +1,13 @@
+// function person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   console.log(this.name);
+// }
+
+// const Tusar = new person("Tusar", 24)
+// console.log(Tusar);
+// const Rakib = new person("Rakib", 31)
+// console.dir(Rakib);
+
+// const f = function Person() {};
+// console.dir(f);
