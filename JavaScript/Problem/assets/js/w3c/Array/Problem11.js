a7b23a29e75a25860ff1db7a1118cdb0f81078b8
Tusar <tusarahmed141@gmail.com>
2023-11-22 09:53:55 +0600

    Another important problem solved

--- JavaScript/Problem/assets/js/w3c/Array/Problem11.js
+++ JavaScript/Problem/assets/js/w3c/Array/Problem11.js
@@ -1,18 +1,42 @@
 // 11. Write a JavaScript program to find the sum of squares of a numerical vector.
 
 
+// const square = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ]
+
+// let sum = 0;
+
+// for (let i = 0; i < square.length; i++) {
+//   for (let j = 0; j < square[i].length; j++) {
+//     sum += square[i][j] ** 2;   
+//   }
+// }
+
+// console.log(`Sum of Square is = ${sum}`);
+
+
 const square = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+  [0, 1, 1],
+  [1, 1, 0],
+  [0, 1, 0],
 ]
 
-let sum = 0;
+const square2 = [
+  [],[],[]
+]
 
 for (let i = 0; i < square.length; i++) {
   for (let j = 0; j < square[i].length; j++) {
-    sum += square[i][j] ** 2;   
+    if (square[i][j] == 1) {
+      square2[i][j] = 2;
+    } else {
+      square2[i][j] = square[i][j];
+    }
   }
 }
 
-console.log(`Sum of Square is = ${sum}`);
+console.log(square);
+console.log(square2);
