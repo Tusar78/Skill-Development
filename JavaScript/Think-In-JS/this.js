c9aca89f829a6c78b51d08ba61155258c48538af
Tusar <tusarahmed141@gmail.com>
2023-11-22 18:27:18 +0600

    Explicit call

--- JavaScript/Think-In-JS/this.js
+++ JavaScript/Think-In-JS/this.js
@@ -5,32 +5,45 @@
  * 4. Window Rule
  */
 
-const person = {
-  name: 'tusar',
-  age: 24,
-  dispFunc: function () {
-    console.log(`${this.name}, How are you?`);
-  }
-}
+// const person = {
+//   name: 'tusar',
+//   age: 24,
+//   dispFunc: function () {
+//     console.log(`${this.name}, How are you?`);
+//   }
+// }
 
-const objWrap = function(obj) {
-  obj.displayName = function () {
-    console.log(`${this.name}, How are you?`);
-  }
+// const objWrap = function(obj) {
+//   obj.displayName = function () {
+//     console.log(`${this.name}, How are you?`);
+//   }
+// }
+
+// const sakib = {
+//   name: 'Sakib',
+//   age: 35
+// }
+
+// const tamim = {
+//   name: 'Tamim',
+//   age: 37,
+// }
+// // person.dispFunc();
+
+// objWrap(sakib);
+// objWrap(tamim);
+// sakib.displayName();
+// tamim.displayName();
+
+
+const myFunc = function () {
+  console.log(this.name);
 }
 
 const sakib = {
-  name: 'Sakib',
-  age: 35
+  name : 'Sakib',
+  age : 37,
 }
 
-const tamim = {
-  name: 'Tamim',
-  age: 37,
-}
-// person.dispFunc();
-
-objWrap(sakib);
-objWrap(tamim);
-sakib.displayName();
-tamim.displayName();
+myFunc.call(sakib);
+sakib.name
