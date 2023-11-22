f97e15017c9b18ff66a10f2b051fddba7f6e5e88
Tusar <tusarahmed141@gmail.com>
2023-11-22 18:24:01 +0600

    Implicit binding done

---
+++ JavaScript/Think-In-JS/this.js
@@ -0,0 +1,36 @@
+/** This Keyword Rule
+ * 1. Implicit Binding
+ * 2. Explicit Binding
+ * 3. New Keyword Rule
+ * 4. Window Rule
+ */
+
+const person = {
+  name: 'tusar',
+  age: 24,
+  dispFunc: function () {
+    console.log(`${this.name}, How are you?`);
+  }
+}
+
+const objWrap = function(obj) {
+  obj.displayName = function () {
+    console.log(`${this.name}, How are you?`);
+  }
+}
+
+const sakib = {
+  name: 'Sakib',
+  age: 35
+}
+
+const tamim = {
+  name: 'Tamim',
+  age: 37,
+}
+// person.dispFunc();
+
+objWrap(sakib);
+objWrap(tamim);
+sakib.displayName();
+tamim.displayName();
