5eda9f2244628a414c7489527cd23934987a8cc0
Tusar <tusarahmed141@gmail.com>
2023-11-22 09:38:57 +0600

    Another problem is solved

---
+++ JavaScript/Problem/assets/js/w3c/Array/Problem10.js
@@ -0,0 +1,26 @@
+// 10. Write a JavaScript program that prints the elements of the following array.
+// Sample array : var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+/*
+Sample Output :
+"row 0"
+" 1"
+" 2"
+" 1"
+" 24"
+"row 1"
+*/
+
+const arr = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(`Row ${i+1}`);
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log(` ${j}`);
+  }  
+}
