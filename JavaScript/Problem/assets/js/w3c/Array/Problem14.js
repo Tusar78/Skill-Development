d388f4cbb8a5541a7555600fc58dd0986c2c3997
Tusar <tusarahmed141@gmail.com>
2023-11-23 11:31:29 +0600

    Duplicate item remove problem solved

--- JavaScript/Problem/assets/js/w3c/Array/Problem14.js
+++ JavaScript/Problem/assets/js/w3c/Array/Problem14.js
@@ -3,13 +3,45 @@
 const arr = [1, 2, 3, 5, 2, 3, 7, 9, 1];
 console.log(arr);
 
-const newArray = [];
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i+1; j < arr.length; j++) {
+//     if (arr[i] == arr[j]) {
+//       arr.splice(j, 1)
+//     }
+//   }  
+// }
+
+// console.log(arr);
+
+// Way 01 - For Loop
+// const repeatItemRemove = arr => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] == arr[j]) {
+//         arr.splice(j, 1);
+//       }
+//     }    
+//   }
+// }
+
+// repeatItemRemove(arr);
+// console.log(arr);
+
+// Way 02 - Includes Method
+// const newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (newArr.includes(arr[i]) != true) {
+//     newArr.push(arr[i])
+//   }
+// } 
+// console.log(newArr);
+
+// Way 03 - Index Of
+const newArr = [];
 for (let i = 0; i < arr.length; i++) {
-  for (let j = i+1; j < arr.length; j++) {
-    if (arr[i] == arr[j]) {
-      arr.splice(j, 1)
-    }
+  if (newArr.indexOf(arr[i]) == -1) {
+    newArr.push(arr[i])
   }  
 }
 
-console.log(arr);
+console.log(newArr);
