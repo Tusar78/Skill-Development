f5393bb0cf18669b49160ce33b2176bf0bc633e8
Tusar <tusarahmed141@gmail.com>
2023-11-26 05:49:15 +0600

    Exploring Array suffle problem

---
+++ JavaScript/Problem/assets/js/w3c/Array/Problem17.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to shuffle an array.
+
+const arr = [1, 2, 3, 4, 5, 7, 8, 9];
+
+const newArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+  newArr.push(arr[i]);  
+}
