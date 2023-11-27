83d4a95131a7a3a6e16246835caa6ce27cd4d33c
Tusar <tusarahmed141@gmail.com>
2023-11-27 14:53:18 +0600

    Getting duplicate arary

---
+++ JavaScript/Problem/assets/js/w3c/Array/Problem20.js
@@ -0,0 +1,19 @@
+/*
+20. Write a JavaScript program to find duplicate values in a JavaScript array.
+*/
+
+const array = [1, 5, 7, 1, 8, 5, 8];
+let len = array.length;
+
+const duplicate = [];
+
+for (let i = 0; i < len; i++) {
+  for (let j = i + 1; j < len; j++) {
+    if (array[i] == array[j]) {
+      duplicate.push(array[i]);
+    }    
+  }  
+}
+
+console.log(array);
+console.log(duplicate);
