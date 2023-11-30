d5a8a54a0271af642448d89bfe780f6e1f31661e
Tusar <tusarahmed141@gmail.com>
2023-11-30 09:10:17 +0600

    Sum of digit pring

--- JavaScript/Problem/assets/js/Anisul_Islam/sum-of-digit.js
+++ JavaScript/Problem/assets/js/Anisul_Islam/sum-of-digit.js
@@ -1,35 +1,66 @@
 // Write a program to sum a program which given
 
-let num = +prompt("Enter the number: ");
+// let num = +prompt("Enter the number: ");
+let num = 153;
 
-const getSum = num => {
+// const getSum = num => {
+//   let temp = num;
+//   let sum = 0;
+//   let sum2 =[];
+//   let r;
+
+//   while (temp != 0) {
+//     r = temp % 10;
+//     sum += r;
+//     sum2.unshift(r);
+//     temp = Math.trunc(temp / 10);
+//   }
+
+//   return [sum, sum2];
+// }
+
+// let [sum, sum2] = getSum(num);
+
+// let pr = '';
+// for (const s of sum2) {
+//   pr += s + ' + ';
+// }
+
+// // console.log(pr.slice(0, -2));
+
+// let presentation = '';
+// for (let i = 0; i < pr.length-2; i++) {
+//     presentation += pr[i];
+// }
+// console.log(presentation);
+// console.log(sum);
+
+const subDigit = num => {
   let temp = num;
   let sum = 0;
-  let sum2 =[];
-  let r;
+  let present = [];
+  let result;
 
   while (temp != 0) {
-    r = temp % 10;
-    sum += r;
-    sum2.push(r);
+    result = temp % 10;
+    sum += result;
+    present.unshift(result);
     temp = Math.trunc(temp / 10);
   }
-
-  return [sum, sum2];
+  
+  return {present, sum};
 }
 
-let [sum, sum2] = getSum(num);
-
-let pr = '';
-for (const s of sum2) {
-  pr += s + ' + ';
+const {present, sum} = subDigit(num);
+let result = '';
+if (present.length) {
+  for (let i = 0; i < present.length; i++) {
+    if (i == present.length-1) {
+      result += present[i];
+    } else {
+      result += present[i] + ' + ';
+    }
+  }
 }
 
-// console.log(pr.slice(0, -2));
-
-let presentation = '';
-for (let i = 0; i < pr.length-2; i++) {
-    presentation += pr[i];
-}
-console.log(presentation);
-console.log(sum);
+console.log(result);
