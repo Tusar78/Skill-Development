66ecc519358b5edf97c8ffa19c86780f0593e6e5
Tusar <tusarahmed141@gmail.com>
2024-02-18 12:47:03 +0600

    Static method done

--- JavaScript/Think-In-JS/Prototype/class.js
+++ JavaScript/Think-In-JS/Prototype/class.js
@@ -226,3 +226,27 @@
 // tusar.getName;
 // tusar.setName = 'Rakib';
 // console.log(tusar.name);
+
+// Static Method
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  eat() {
+    console.log(`${this.name} can eat`);
+  }
+
+  static myStaticFunction(p1, p2) {
+    if (p1.age == p2.age) {
+      console.log('Two person age are equal');
+    } else {
+      console.log('Two person age are not equal');
+    }
+  }
+}
+
+const tusar = new Person('Tusar', 24);
+const rakib = new Person("Rakib", 31);
+Person.myStaticFunction(tusar, rakib);
