c3b96b6b49a8818ef7eb494c05a595b50ffc961d
Tusar <tusarahmed141@gmail.com>
2024-02-18 14:41:08 +0600

    Series complete

--- JavaScript/Problem/assets/js/seriese/seriese1.js
+++ JavaScript/Problem/assets/js/seriese/seriese1.js
@@ -1,20 +1,42 @@
 //Series  
 // 1 + 2 + 3 + 4 + .... + n;
 
+// let num = +prompt("Enter any number: ");
+
+// const getSeries = num => {
+//   let sum = 0;
+//   let series = '';
+
+//   for (let i = 1; i <= num; i++) {
+//       sum += i;
+//       series += i + ' + ';
+//   }
+
+//   return {sum, series};
+// }
+
+// const {sum, series} = getSeries(num);
+// console.log(series.slice(0, -2));
+// console.log(sum);
+
+
+//Series  
+// 1 + 2 + 3 + 4 + .... + n = sum;
+
 let num = +prompt("Enter any number: ");
 
 const getSeries = num => {
   let sum = 0;
-  let series = '';
+  let series = ''; 
 
   for (let i = 1; i <= num; i++) {
-      sum += i;
-      series += i + ' + ';
+    sum += i;
+    i == num ? series += i : series += i + ' + ';
   }
 
   return {sum, series};
 }
 
 const {sum, series} = getSeries(num);
-console.log(series.slice(0, -2));
 console.log(sum);
+console.log(series);
