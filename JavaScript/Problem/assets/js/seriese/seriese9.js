9b3fd1a32dad74c78c019ba09d409ffe3ee94f89
Tusar <tusarahmed141@gmail.com>
2024-02-18 18:04:07 +0600

    Another interesting series is done

--- JavaScript/Problem/assets/js/seriese/seriese9.js
+++ JavaScript/Problem/assets/js/seriese/seriese9.js
@@ -1,29 +1,54 @@
 // Series : 1 - 2 + 3 - 4 + 5 - 6 + ......+-n;
 
-let n = +prompt("Enter your number");
+// let n = +prompt("Enter your number");
 
-const getSeries = (n) => {
+// const getSeries = (n) => {
+//   let sum = 0;
+//   let series = "";
+
+//   for (let i = 1; i <= n; i++) {
+//     if (i == 1) {
+//       series += i;
+//       sum += i;
+//     } else if (i % 2 != 0) {
+      
+//       series += " + " + i;
+//       sum += i;
+//     } else {
+//       series += " - " + i;
+//       sum -= i;
+//     }
+//   }
+
+//   return { sum, series };
+// };
+
+// let { sum, series } = getSeries(n);
+
+// Print Series
+// console.log(`${series} = ${sum}`);
+
+const num = prompt("Enter your number: ");
+
+const getSeries = num => {
   let sum = 0;
-  let series = "";
+  let series = '';
 
-  for (let i = 1; i <= n; i++) {
+  for (let i = 1; i <= num; i++) {
     if (i == 1) {
       series += i;
       sum += i;
     } else if (i % 2 != 0) {
-      
-      series += " + " + i;
+      series += ' + ' + i;
       sum += i;
     } else {
-      series += " - " + i;
+      series += ' - ' + i;
       sum -= i;
     }
   }
+  return {sum, series};
+}
 
-  return { sum, series };
-};
-
-let { sum, series } = getSeries(n);
-
-// Print Series
-console.log(`${series} = ${sum}`);
+const {sum, series} = getSeries(num);
+const result = `${series} = ${sum}`;
+console.log(result);
