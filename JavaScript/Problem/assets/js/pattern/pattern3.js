209c9227a036ae6019ce278e72274829b936034b
Tusar <tusarahmed141@gmail.com>
2024-02-19 10:52:42 +0600

    Triangle binary pattern done

--- JavaScript/Problem/assets/js/pattern/pattern3.js
+++ JavaScript/Problem/assets/js/pattern/pattern3.js
@@ -7,17 +7,47 @@
 
  let n = +prompt("Enter your row number: ");
 
- const patternTwo = n => {
-   let pattern = '';
-   for (let i = 1; i <= n; i++) {
-     for (let j = 1; j <= i; j++) {
-       pattern += (j % 2) + ' ';
-     }
-     pattern += '\n';
-   }
+//  const patternTwo = n => {
+//    let pattern = '';
+//    for (let i = 1; i <= n; i++) {
+//      for (let j = 1; j <= i; j++) {
+//        pattern += (j % 2) + ' ';
+//      }
+//      pattern += '\n';
+//    }
  
-   return {pattern};
- }
+//    return {pattern};
+//  }
  
- const {pattern} = patternTwo(n);
- console.log(pattern);
+//  const {pattern} = patternTwo(n);
+//  console.log(pattern);
+
+// const getPattern = num => {
+//   let pattern = '';
+//   for (let i = 1; i <= num; i++) {
+//     for (let j = 1; j <= i; j++) {
+//       pattern += j % 2 != 0 ? j : j % 2;
+//     }
+//     pattern += '\n';
+//   }
+
+//   return {pattern}
+// }
+
+// const {pattern} = getPattern(n);
+// console.log(pattern);
+
+const getPattern = num => {
+  let pattern = '';
+  for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= i; j++) {
+      pattern += i % 2;
+    }
+    pattern += '\n';
+  }
+
+  return {pattern}
+}
+
+const {pattern} = getPattern(n);
+console.log(pattern);
