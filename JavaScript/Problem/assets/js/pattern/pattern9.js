e74fac68240519153bb7bfbfbfecd61f6b3ad055
Tusar <tusarahmed141@gmail.com>
2024-02-20 10:41:36 +0600

    Another special triangle pattern is done

--- JavaScript/Problem/assets/js/pattern/pattern9.js
+++ JavaScript/Problem/assets/js/pattern/pattern9.js
@@ -9,24 +9,43 @@ x x x x x
 */
 
 let rows = +prompt(
-  "Enter How Many Rows You Want to see, 'Press between 1 to 9'"
+  "Enter How Many Rows You Want to see, 'Press Enter between 1 to 9'"
 );
 
-const patternOne = (n) => {
-  let pattern = "";
+// const patternOne = (n) => {
+//   let pattern = "";
+//   for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= n; j++) {
+//       if (i == n || j == 1 || j == i) {
+//         pattern += "x" + " ";      
+//       } else {
+//         pattern += "  ";      
+//       }
+//     }
+//     pattern += "\n";
+//   }
+
+//   return { pattern };
+// };
+
+// let {pattern} = patternOne(rows);
+// console.log(pattern);
+
+const getPattern = n => {
+  let pattern = '';
   for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (i == n || j == 1 || j == i) {
-        pattern += "x" + " ";      
+    for (let j = 1; j <= i; j++) {
+      if (j == 1 || i == n || j == i) {
+        pattern += 'x' + ' ';
       } else {
-        pattern += "  ";      
+        pattern += '-' + ' ';
       }
     }
-    pattern += "\n";
+    pattern += '\n';
   }
 
-  return { pattern };
-};
+  return {pattern};
+}
 
-let {pattern} = patternOne(rows);
+const {pattern} = getPattern(rows);
 console.log(pattern);
