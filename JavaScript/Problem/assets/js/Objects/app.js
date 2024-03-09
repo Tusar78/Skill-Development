c33bd34b662114a5cd6f2c3da8018aaf68e554ec
Tusar <tusarahmed141@gmail.com>
2024-03-09 15:51:13 +0600

    Last update for object

--- JavaScript/Problem/assets/js/Objects/app.js
+++ JavaScript/Problem/assets/js/Objects/app.js
@@ -1,20 +1,20 @@
-const obj1 = {
-    name: 'Tusar',
-    email: 'tusar@gmail.com'
-}
+// const obj1 = {
+//     name: 'Tusar',
+//     email: 'tusar@gmail.com'
+// }
 
-const obj2 = {
-    name1: 'Rakib',
-    email1: 'rakib@gmail.com'
-}
+// const obj2 = {
+//     name1: 'Rakib',
+//     email1: 'rakib@gmail.com'
+// }
 
-// const obj3 = Object.assign(obj1, obj2);
-// const obj3 = {...obj1, ...obj2}
-const objKeys = Object.keys(obj1);
-const objValues = Object.values(obj2);
-const objEntries = Object.entries(obj1);
-// console.log(objEntries);
+// // const obj3 = Object.assign(obj1, obj2);
+// // const obj3 = {...obj1, ...obj2}
+// const objKeys = Object.keys(obj1);
+// const objValues = Object.values(obj2);
+// const objEntries = Object.entries(obj1);
+// // console.log(objEntries);
 
-for (let i = 0; i < objEntries.length; i++) {
-    console.log(objEntries[i][i]);    
-}
+// for (let i = 0; i < objEntries.length; i++) {
+//     console.log(objEntries[i][i]);    
+// }
