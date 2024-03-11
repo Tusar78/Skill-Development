d507e6f64d8ca523816651fa00cc24f4c3129c5a
Tusar <tusarahmed141@gmail.com>
2024-03-11 18:09:18 +0600

    Last update for get and set method

--- JavaScript/Problem/assets/js/OOPInJS/app.js
+++ JavaScript/Problem/assets/js/OOPInJS/app.js
@@ -3,23 +3,50 @@ person.name = "Tusar";
 person.email = "tusar@gmail.com";
 // console.log(person);
 
+// class Person {
+//     constructor(name, email) {
+//         this.name = name;
+//         this.email = email;
+//     }
+
+//     nameChange(name) {
+//         this.name = name;
+//     }
+
+//     sendMsg(msg) {
+//         console.log(`${msg} ${this.name}`);
+//         console.log(`send to ${this.email}`);
+//     }
+// }
+
+// const p1 = new Person("tusar", "tus@gmail.com");
+// const p2 = new Person("Rakib", "rakib@gmail.com");
+
+// p2.sendMsg("Hello");
+
 class Person {
     constructor(name, email) {
-        this.name = name;
-        this.email = email;
+        this._name = name;
+        this._email = email;
     }
 
-    nameChange(name) {
-        this.name = name;
+    setName(name) {
+        this._name = name;
     }
 
-    sendMsg(msg) {
-        console.log(`${msg} ${this.name}`);
-        console.log(`send to ${this.email}`);
+    getName() {
+        return this._name;
+    }
+
+    print() {
+        console.log(this);
     }
 }
 
-const p1 = new Person("tusar", "tus@gmail.com");
+const p1 = new Person("Tusar", "tusar@gmail.com");
 const p2 = new Person("Rakib", "rakib@gmail.com");
 
-p2.sendMsg("Hello");
+console.log(p1.getName());
+p1.setName("Tusar Ali");
+console.log(p1.getName());
+p1.print();
