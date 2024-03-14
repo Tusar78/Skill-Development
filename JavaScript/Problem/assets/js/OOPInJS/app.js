acf39a80daa648894579a7d998c3e3b19fb97811
Tusar <tusarahmed141@gmail.com>
2024-03-14 12:51:02 +0600

    created object based on

--- JavaScript/Problem/assets/js/OOPInJS/app.js
+++ JavaScript/Problem/assets/js/OOPInJS/app.js
@@ -81,47 +81,67 @@ person.email = "tusar@gmail.com";
 // p2.sendMsg("        Wow! Your looking so cool.   ");
 
 // Getter and Setter
-class Person {
-    constructor(name, email) {
-        this._name = name;
-        this._email = email;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    set name(value) {
-        if (value === 'Tus') {
-            return
-        } else {
-            this._name = value;
-        }
-    }
-}
-
-const p1 = new Person('Tusar', 'tusar@gmail.com');
-const p2 = new Person('Rakib', 'rakib@gmail.com');
-
-console.log(p2.name);
-p2.name = 'Tus';
-
-console.log(p2.name);
-
 // class Person {
 //     constructor(name, email) {
 //         this._name = name;
 //         this._email = email;
 //     }
 
-//     print() {
-//         console.log(this + '');
+//     get name() {
+//         return this._name;
 //     }
 
-//     toString() {
-//         return `Name: ${this._name}, Email: ${this._email}`
+//     set name(value) {
+//         if (value === 'Tus') {
+//             return
+//         } else {
+//             this._name = value;
+//         }
 //     }
 // }
 
 // const p1 = new Person('Tusar', 'tusar@gmail.com');
-// p1.print();
+// const p2 = new Person('Rakib', 'rakib@gmail.com');
+
+// console.log(p2.name);
+// p2.name = 'Tus';
+
+// console.log(p2.name);
+
+class Person {
+    static className = 'Person';
+    static PI = 3.14159;
+
+    constructor(name, email) {
+        this._name = name;
+        this._email = email;
+    }
+
+    print() {
+        console.log(this + '');
+    }
+
+    toString() {
+        return `Name: ${this._name}, Email: ${this._email}`
+    }
+
+    static isValid(age) {
+        return age >= 18;
+    }
+}
+
+const p1 = new Person('Tusar', 'tusar@gmail.com');
+const p2 = new Person('Rakib', 'rakib@gmail.com');
+
+const testObj = {
+    name: 'Aopo',
+    email: 'aopo@gmail.com',
+    age: 15
+}
+
+let p3 = null;
+if (Person.isValid(testObj.age)) {
+    p3 = new Person(testObj.name, testObj.email);
+}
+
+console.log(p3);
