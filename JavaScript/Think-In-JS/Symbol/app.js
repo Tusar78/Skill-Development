b0c7a5be1ca0fb2a5b223a1fa0537848c950f156
Tusar <tusarahmed141@gmail.com>
2024-03-16 16:53:47 +0600

    Using some symbol feature

--- JavaScript/Think-In-JS/Symbol/app.js
+++ JavaScript/Think-In-JS/Symbol/app.js
@@ -3,3 +3,11 @@ const symbol2 = Symbol.for('Tusar');
 console.log(symbol1===symbol2);
 console.log(symbol1);
 console.log(symbol2);
+
+const name = Symbol('name');
+const person = {
+	[name]: 'Tusar',
+	email: 'tusar@gmail.com'
+}
+
+console.log(person);
