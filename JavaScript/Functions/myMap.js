f16d602b7ca07b1e1bc38cfd0a88e5ccac29f608
Tusar <tusarahmed141@gmail.com>
2024-03-23 21:42:30 +0600

    Map method is so easy but workful

---
+++ JavaScript/Functions/myMap.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// Travarse and genereate new array using map method
+const fruits2 = fruits.map((val, i, arr) => {
+    return val + 0;
+}) 
+
