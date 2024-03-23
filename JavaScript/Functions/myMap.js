cf9ae8cdaade61a154ad5b52ea78fdfafca6fd96
Tusar <tusarahmed141@gmail.com>
2024-03-23 21:56:01 +0600

    Implemented on myMAp

--- JavaScript/Functions/myMap.js
+++ JavaScript/Functions/myMap.js
@@ -1,31 +1,44 @@
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-// Travarse and genereate new array using map method
-const fruits2 = fruits.map((val, i, arr) => {
-    return val;
-}) 
+// // Travarse and genereate new array using map method
+// const fruits2 = fruits.map((val, i, arr) => {
+//     return val;
+// }) 
 
-// Travarse Fruits on for lop
-const fruits3 = [];
-for (let i = 0; i < fruits.length; i++) {
-    fruits3[i] = fruits[i] + 0;
-}
+// // Travarse Fruits on for lop
+// const fruits3 = [];
+// for (let i = 0; i < fruits.length; i++) {
+//     fruits3[i] = fruits[i] + 0;
+// }
 
-console.log(`First Array: ${fruits}`); 
-console.log(`Third Array: ${fruits3}`); 
+// console.log(`First Array: ${fruits}`); 
+// console.log(`Third Array: ${fruits3}`); 
 
-// Let's make a my map
-function myMap(arr, cb) {
+// // Let's make a my map
+// function myMap(arr, cb) {
+//     let newArray = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         newArray[i] = cb(arr[i], i, arr);
+//     }
+
+//     return newArray;
+// }
+
+// const myFruits = myMap(fruits, (val, i, arr) => {
+//     return val + (i+1);
+// })
+
+// console.log(myFruits);
+
+// Create myMap on Array.prototype
+Array.prototype.myMap = function(cb){
     let newArray = [];
-    for (let i = 0; i < arr.length; i++) {
-        newArray[i] = cb(arr[i], i, arr);
+    for (let i = 0; i < this.length; i++) {
+        newArray[i] = cb(this[i], i, this);
     }
-
     return newArray;
 }
-
-const myFruits = myMap(fruits, (val, i, arr) => {
-    return val + (i+1);
+const myF = fruits.myMap((val, i, arr) => {
+    return val + (i+1)
 })
-
-console.log(myFruits);
+console.log(myF);
