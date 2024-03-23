07405f1b15fa1fa1423b26d409429ba3b475f672
Tusar <tusarahmed141@gmail.com>
2024-03-23 21:44:52 +0600

    We travarse a array elements and genereate a new array

--- JavaScript/Functions/myMap.js
+++ JavaScript/Functions/myMap.js
@@ -5,3 +5,11 @@ const fruits2 = fruits.map((val, i, arr) => {
     return val + 0;
 }) 
 
+// Travarse Fruits on for lop
+const fruits3 = [];
+for (let i = 0; i < fruits.length; i++) {
+    fruits3[i] = fruits[i] + 0;
+}
+
+console.log(`First Array: ${fruits}`); 
+console.log(`Third Array: ${fruits3}`); 
