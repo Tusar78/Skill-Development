1029d1750905eea7690f27c0941d9e87b27ba087
Tusar <tusarahmed141@gmail.com>
2024-03-25 14:47:50 +0600

    XML http request using callback function

--- JavaScript/Asynchronous/app.js
+++ JavaScript/Asynchronous/app.js
@@ -8,18 +8,46 @@
 // console.log(output);
 
 // Handle ajax using callback function
-const xhr = new XMLHttpRequest();
-console.log(xhr);
-xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
-xhr.onreadystatechange = function (e) {
-    if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-            console.log(xhr.response);
-        } else {
-            console.log(xhr.status);
+// const xhr = new XMLHttpRequest();
+// console.log(xhr);
+// xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
+// xhr.onreadystatechange = function (e) {
+//     if (xhr.readyState === 4) {
+//         if (xhr.status === 200) {
+//             console.log(xhr.response);
+//         } else {
+//             console.log(xhr.status);
+//         }
+//     }
+// }
+
+// xhr.send();
+// console.log('Tusar');
+
+function getRequest(url, cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+
+    xhr.onreadystatechange = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                let response = JSON.parse(xhr.response);
+                cb(null, response);
+            } else {
+                cb(xhr.status, null);
+            }
         }
     }
+    
+    xhr.send();
 }
 
-xhr.send();
-console.log('Tusar');
+getRequest('https://jsonplaceholder.typicode.com/users', (err, res) => {
+    if (err) {
+        console.log(err);
+    } else {
+        res.forEach(element => {
+          console.log(element.username);
+        });
+    }
+})
