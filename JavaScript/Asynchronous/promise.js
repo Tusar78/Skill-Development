f6624d3e0d77db4b9d8d1820f82983916c30d497
Tusar <tusarahmed141@gmail.com>
2024-03-26 11:38:01 +0600

    Created our own promise

--- JavaScript/Asynchronous/promise.js
+++ JavaScript/Asynchronous/promise.js
@@ -1,21 +1,40 @@
-const p1 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 5000, 'One');
-})
+// const p1 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 5000, 'One');
+// })
 
-console.log(p1);
+// console.log(p1);
 
-const p2 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 3000, 'Two');
-})
+// const p2 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 3000, 'Two');
+// })
 
-console.log(p2);
+// console.log(p2);
 
-p1.then(e => {
-    console.log(e);
+// p1.then(e => {
+//     console.log(e);
+// }).catch(e => {
+//     console.log(e);
+// })
+
+// p2.then(e => {
+//     console.log(e);
+// })
+
+function getPromise(value) {
+    return new Promise((resolve, reject) => {
+        if (value) {
+            setTimeout(() => {
+                resolve('Yes, this is resolved');
+            }, 3000)
+        } else {
+            reject(new Error('No, you not get!'))
+        }
+    })
+}
+
+const p1 = getPromise(true);
+p1.then(v => {
+    console.log(v);
 }).catch(e => {
-    console.log(e);
-})
-
-p2.then(e => {
-    console.log(e);
+    console.log(e.message);
 })
