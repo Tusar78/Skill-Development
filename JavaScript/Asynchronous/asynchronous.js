bf20c32bb346d54e516350c0c7a1a2b711844374
Tusar <tusarahmed141@gmail.com>
2024-05-03 15:31:47 +0600

    Add one important file

--- JavaScript/Asynchronous/asynchronous.js
+++ JavaScript/Asynchronous/asynchronous.js
@@ -68,14 +68,17 @@
 //     console.log(err.message);
 //   });
 
-
 // Promise.all and Promise.race
-const promise1 = Promise.resolve("Promise 1 Resolve");
-const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("Promise 2 Resolve")
-    }, 3000);
-})
+// const promise1 = Promise.resolve("Promise 1 Resolve");
+// const promise2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise 2 Resolve");
+//   }, 3000);
+// });
 
-promise1.then(res => console.log(res))
-promise2.then(res => console.log(res))
+// promise1.then(res => console.log(res))
+// promise2.then(res => console.log(res))
+
+// Promise.all([promise1, promise2]).then((res) => console.log(res));
+// Promise.race([promise1, promise2]).then((res) => console.log(res));
+
