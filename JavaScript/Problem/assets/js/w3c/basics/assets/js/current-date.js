0a56fb9761ac2dca0b5ec55ed3ebf3d7a90869ad
Tusar <tusarahmed141@gmail.com>
2024-06-03 23:20:11 +0600

    Current date done

--- JavaScript/Problem/assets/js/w3c/basics/assets/js/current-date.js
+++ JavaScript/Problem/assets/js/w3c/basics/assets/js/current-date.js
@@ -1,4 +1,14 @@
 const dateContainer = document.querySelector(".date");
 
-// Date Object 
-const DATE = new Date();
+// Date Generate
+const getDT = () => {
+  // Date Object
+  const DATE = new Date();
+  const day = DATE.getDate();
+  const month = DATE.getMonth();
+  const year = DATE.getFullYear();
+
+  dateContainer.innerText = `${day < 10 ? `0${day}` : `${day}` } - ${month < 10 ? `0${month}` : `${month}` } - ${year}`
+};
+
+getDT();
