58ffd47c8d7e98aa3e1394b6418b9d8f512eb321
Tusar <tusarahmed141@gmail.com>
2024-06-08 22:17:34 +0600

    Solve one simple conditional problem

--- JavaScript/Problem/assets/js/w3c/basics/assets/js/rotate-string.js
+++ JavaScript/Problem/assets/js/w3c/basics/assets/js/rotate-string.js
@@ -3,12 +3,37 @@ Write a JavaScript program to rotate the string 'w3resource' in the right direct
 This is done by periodically removing one letter from the string end and attaching it to the front.  
 */
 
-const rotateStr = document.querySelector('.rotate-str');
-let textNode = rotateStr.childNodes[0];
-let text = textNode.data;
-setInterval(function () {
-    text = text[text.length - 1] + text.substring(0, text.length-1)
-    textNode.data = text;
-}, 100)
+// const rotateStr = document.querySelector('.rotate-str');
+// let textNode = rotateStr.childNodes[0];
+// let text = textNode.data;
+// setInterval(function () {
+//     text = text[text.length - 1] + text.substring(0, text.length-1)
+//     textNode.data = text;
+// }, 100)
+
+
+const n = 4;
+if (n > 9) {
+    cout << "Greater than 9";
+} else if(n == 9) {
+    cout << "nine";
+}else if(n == 8) {
+    cout << "eight";
+}else if(n == 7) {
+    cout << "seven";
+}else if(n == 6) {
+    cout << "six";
+}else if(n == 5) {
+    cout << "five";
+}else if(n == 4) {
+    cout << "four";
+}else if(n == 3) {
+    cout << "three";
+}else if(n == 2) {
+    cout << "two";
+}else if(n == 1) {
+    cout << "one";
+}
+
 
 
