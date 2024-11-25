d9ca8c8334a146cbebeb78cfdb93708c934a9e1e
Tusar <tusarahmed141@gmail.com>
2024-11-25 07:08:51 +0600

    Solve another unique problem called find factorial

--- JavaScript/DailyProblem/day_03.js
+++ JavaScript/DailyProblem/day_03.js
@@ -47,3 +47,42 @@ const findSecondLargest = arr => {
 
 const result7 = findSecondLargest([3, 5, 7, 5, 6]);
 console.log(result7);
+
+
+/*
+Problem 8: Find the Factorial of a Number
+Write a function named findFactorial that calculates the factorial of a given non-negative integer. The factorial of a number n is the product of all positive integers less than or equal to n.
+
+Conditions
+-----------
+    1. If the input number is 0, the factorial is 1.
+    2. Return null if the input is a negative number.
+    3. Factorial of a number n is calculated as n * (n-1) * (n-2) * ... * 1.
+
+Examples
+--------
+    findFactorial(5); 
+    // Output: 120 (5 * 4 * 3 * 2 * 1)
+
+    findFactorial(0); 
+    // Output: 1
+
+    findFactorial(-3); 
+    // Output: null
+
+*/
+
+const findFactorial = n => {
+    if(n < 0) return null; // Edge case
+    if (n === 0) return 1; // Base Case
+
+    let fact = 1;
+    for (let i = n; i > 1; i--) {
+        fact *= i; 
+    }
+
+    return fact;
+}
+
+const result8 = findFactorial(5);
+console.log(result8);
