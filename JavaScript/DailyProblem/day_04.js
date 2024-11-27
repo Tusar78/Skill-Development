2b1557a4fb1aed9961047cd4427115792183a08d
Tusar <tusarahmed141@gmail.com>
2024-11-27 13:09:40 +0600

    Solve vowel count problem

--- JavaScript/DailyProblem/day_04.js
+++ JavaScript/DailyProblem/day_04.js
@@ -52,3 +52,95 @@ function isAnagram(str1, str2) {
 const result10 = isAnagram("The eyes", "They see");
 console.log(result10);
 
+
+/*
+Problem 11: Check if an Array is Sorted
+Write a function to check whether a given array of numbers is sorted in ascending order.
+
+Function Signature
+---------------
+function isSorted(arr) {
+    // Your code here
+}
+  Example Test Cases
+  Input: [1, 2, 3, 4, 5]
+  Output: true
+  (Explanation: The array is sorted in ascending order.)
+
+  Input: [5, 3, 8, 1]
+  Output: false
+  (Explanation: The array is not sorted in ascending order.)
+
+  Input: [10, 20, 30, 40]
+  Output: true
+
+  Input: [10, 20, 15, 30]
+  Output: false
+
+  Input: []
+  Output: true
+  (Explanation: An empty array is considered sorted.)
+*/
+
+const isSorted = arr => {
+  // Edge case
+  if(!Array.isArray(arr)) return `Enter only array`;
+
+  if (arr.length === 0) {
+    return true;
+  }
+
+  const newArr = arr.join('');
+  const sortedArr = [...arr].sort((a, b) => a - b).join('');
+  
+  return newArr === sortedArr
+}
+
+const result11 = isSorted([10, 20, 30, 40]);
+console.log(result11);
+
+
+/*
+Problem 12: Count Vowels in a String
+Write a function called countVowels that takes a string as input and returns the number of vowels (a, e, i, o, u) in the string. Both uppercase and lowercase vowels should be counted.
+
+Input
+A string (e.g., "Hello World!").
+
+Output
+------
+A number representing the count of vowels in the string (e.g., 3).
+
+Requirements
+-------------
+  1. Ignore non-alphabetical characters.
+  2. Consider both uppercase and lowercase vowels (a, e, i, o, u).
+  3. Return 0 for empty strings or strings with no vowels.
+
+Examples
+--------
+  countVowels("Hello World!"); // Output: 3
+  countVowels("JavaScript");   // Output: 3
+  countVowels("");             // Output: 0
+  countVowels("rhythm");       // Output: 0
+  countVowels("AEIOUaeiou");   // Output: 10
+*/
+
+const countVowels = str => {
+  // Edge case
+  if (typeof str !== 'string') return `Please give me text`;
+
+  str = str.trim().toLowerCase();  
+  
+  let count = 0;
+  for (const char of str) {    
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+      count += 1;
+    }
+  }
+
+  return count;
+}
+
+const result12 = countVowels("AEIOUaeiou");
+console.log(result12);
